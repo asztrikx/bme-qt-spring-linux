19309a42ccff98b1abb056978696d1d47041532a
asztrikx <asztrik35@gmail.com>
2023-03-18 15:53:45 +0100

    init from start.spring.io

---
+++ src/test/kotlin/hu/vecsesiot/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package hu.vecsesiot.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
