08ffb3999b4a2075578be7fa443af561d108fdca
asztrikx <asztrik35@gmail.com>
2023-03-25 16:35:56 +0100

    add sql initializer

---
+++ src/main/kotlin/hu/vecsesiot/backend/SqlInitializer.kt
@@ -0,0 +1,19 @@
+package hu.vecsesiot.backend
+
+import hu.vecsesiot.backend.user.User
+import hu.vecsesiot.backend.user.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.stereotype.Component
+
+@Component
+class DataLoader : ApplicationRunner {
+	@Autowired
+	private lateinit var userRepository: UserRepository
+
+	override fun run(args: ApplicationArguments) {
+		val user = User(username = "asd")
+		userRepository.save(user)
+	}
+}
