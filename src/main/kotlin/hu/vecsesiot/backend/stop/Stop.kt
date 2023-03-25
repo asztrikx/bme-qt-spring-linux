9da2b765f6759a7987800866bcd2a62861278e56
asztrikx <asztrik35@gmail.com>
2023-03-25 16:34:42 +0100

    formatted

--- src/main/kotlin/hu/vecsesiot/backend/stop/Stop.kt
+++ src/main/kotlin/hu/vecsesiot/backend/stop/Stop.kt
@@ -1,10 +1,7 @@
 package hu.vecsesiot.backend.stop
 
 import hu.vecsesiot.backend.line.Line
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.Id
-import jakarta.persistence.ManyToMany
+import jakarta.persistence.*
 
 @Entity
 class Stop(
@@ -12,8 +9,8 @@ class Stop(
 	@GeneratedValue
 	val id: Long?,
 	val coordinate: GPSCoordinate,
-	val name : String,
+	val name: String,
 	// https://stackoverflow.com/a/14111651/4404911
-	@ManyToMany(mappedBy="stops")
-	val lines : List<Line>
+	@ManyToMany(mappedBy = "stops")
+	val lines: List<Line>
 )
