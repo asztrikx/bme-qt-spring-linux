3fe8411b0e21d183e244c4bf4c430bedb918923b
potyok <potyok.csaba@edu.bme.hu>
2023-03-25 16:50:45 +0100

    Remove redundances

--- src/main/kotlin/hu/vecsesiot/backend/timetable/TimetableService.kt
+++ src/main/kotlin/hu/vecsesiot/backend/timetable/TimetableService.kt
@@ -12,16 +12,10 @@ class TimetableService {
     @Autowired
     private lateinit var repository: TimetableRepository
 
-    @Transactional
-    fun addTimetable(timetable: Timetable) = repository.save(timetable)
 
     fun findTimetablesByLine(line: Line) = repository.findAllByLine(line)
 
     fun findTimetableByLineAndAfterDate(line: Line, date: LocalDateTime) =
         repository.findAllByLineAndAfterDate(line, date)
 
-    fun findById(id: Long) = repository.findById(id)
-
-    @Transactional
-    fun deleteLineById(id: Long) = repository.deleteById(id)
 }
