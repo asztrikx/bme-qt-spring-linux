487e25f81b4b7afdcbbab7c22cff2852d12abb99
Rádai Ronald <gtadj.roni@gmail.com>
2023-03-25 16:55:36 +0100

    FaultTicket Controller and Service set to autogenerated

--- src/main/kotlin/hu/vecsesiot/backend/faultticket/FaultTicketController.kt
+++ src/main/kotlin/hu/vecsesiot/backend/faultticket/FaultTicketController.kt
@@ -14,19 +14,6 @@ class FaultTicketController {
     @Autowired
     private lateinit var service: FaultTicketService
 
-    @PostMapping("")
-    fun addFaultTicket(@RequestBody faultTicket: FaultTicket) = service.addFaultTicket(faultTicket)
-
-    @GetMapping("/{:id}")
-    fun getFaultTicketById(@PathVariable id: Long) = service.findById(id)
-
     @GetMapping("/?user")
     fun getFaultTicketByUser(@RequestBody user: User) = service.findFaultTicketsByUser(user)
-
-    @PutMapping("")
-    fun updateFaultTicket(@RequestBody faultTicket: FaultTicket) =
-        if (faultTicket.id == null) ResponseEntity.notFound() else service.addFaultTicket(faultTicket)
-
-    @DeleteMapping("/{:id}")
-    fun deleteFaultTicketById(@PathVariable id: Long) = service.deleteFaultTicketById(id)
 }
--- src/main/kotlin/hu/vecsesiot/backend/faultticket/FaultTicketService.kt
+++ src/main/kotlin/hu/vecsesiot/backend/faultticket/FaultTicketService.kt
@@ -4,24 +4,11 @@ import hu.vecsesiot.backend.user.User
 import jakarta.transaction.Transactional
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.util.*
-import kotlin.jvm.optionals.getOrNull
 
 @Service
 class FaultTicketService {
     @Autowired
     private lateinit var repository: FaultTicketRepository
 
-    @Transactional
-    fun addFaultTicket(faultTicket : FaultTicket) = repository.save(faultTicket)
-
-    fun findFaultTicketsByUser(user : User) = repository.findAllByUser(user)
-
-    fun findById(id : Long) = repository.findById(id)
-
-    //@Transactional
-    //fun updateFaultTicket(faultTicket : FaultTicket) = repository.save(findById(faultTicket.id!!).getOrNull() ?: throw Exception("404"))
-
-    @Transactional
-    fun deleteFaultTicketById(id : Long) = repository.deleteById(id)
+    fun findFaultTicketsByUser(user: User) = repository.findAllByUser(user)
 }
