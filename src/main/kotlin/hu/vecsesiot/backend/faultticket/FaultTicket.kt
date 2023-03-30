417b99a14c8214cbac969563ff52126046a9878e
potyok <potyok.csaba@edu.bme.hu>
2023-03-30 17:08:13 +0200

    change date to localdatetime

--- src/main/kotlin/hu/vecsesiot/backend/faultticket/FaultTicket.kt
+++ src/main/kotlin/hu/vecsesiot/backend/faultticket/FaultTicket.kt
@@ -5,29 +5,29 @@ import hu.vecsesiot.backend.stop.GPSCoordinate
 import hu.vecsesiot.backend.user.User
 import jakarta.persistence.*
 import jakarta.validation.constraints.NotBlank
-import java.util.*
+import java.time.LocalDateTime
 
 @Entity
 class FaultTicket(
-	@Id
-	@GeneratedValue
-	var id: Long?,
-	var startDate: Date,
-	var resolveDate: Date,
-	@NotBlank
-	var description: String,
-	var coordinate: GPSCoordinate,
-	@Enumerated(EnumType.STRING)
-	var state: State
+    @Id
+    @GeneratedValue
+    var id: Long?,
+    var startDate: LocalDateTime,
+    var resolveDate: LocalDateTime,
+    @NotBlank
+    var description: String,
+    var coordinate: GPSCoordinate,
+    @Enumerated(EnumType.STRING)
+    var state: State
 ) {
-	@ManyToOne
-	lateinit var user: User
+    @ManyToOne
+    lateinit var user: User
 
-	// could be a computed value
-	@ManyToOne
-	lateinit var bus: Bus
+    // could be a computed value
+    @ManyToOne
+    lateinit var bus: Bus
 
-	enum class State {
-		Created, InProgress, Resolved
-	}
+    enum class State {
+        Created, InProgress, Resolved
+    }
 }
