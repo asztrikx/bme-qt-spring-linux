433df8dffef813ffde09f3990e4a65d197bba2c9
asztrikx <asztrik35@gmail.com>
2023-04-02 01:43:48 +0200

    remove unnecessary code

--- src/main/kotlin/hu/vecsesiot/backend/bus/BusRepository.kt
+++ src/main/kotlin/hu/vecsesiot/backend/bus/BusRepository.kt
@@ -5,12 +5,8 @@ import hu.vecsesiot.backend.user.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
-import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-
-@RepositoryRestResource(collectionResourceRel = "buses", path = "buses")
 interface BusRepository : JpaRepository<Bus, Long> {
-	// TODO empty is permitted
 	fun findBySerialnumber(serialnumber: String): Bus?
 	fun findByUser(user: User): Bus?
 
