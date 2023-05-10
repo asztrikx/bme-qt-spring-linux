97afd84121c66adb5154dfcfaf70de004c5e7d0d
asztrikx <asztrik35@gmail.com>
2023-05-10 09:45:46 +0200

    added logging

--- backend/src/main/kotlin/hu/vecsesiot/backend/email/EmailService.kt
+++ backend/src/main/kotlin/hu/vecsesiot/backend/email/EmailService.kt
@@ -1,6 +1,9 @@
 package hu.vecsesiot.backend.email
 
+import hu.vecsesiot.backend.bus.BusController
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.mail.MailException
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
@@ -10,12 +13,22 @@ import org.springframework.stereotype.Service
 class EmailService {
 	@Autowired
 	private lateinit var mailSender: JavaMailSender
+
+	var logger = LoggerFactory.getLogger(BusController::class.java)
+
 	fun sendEmail(to: String, subject: String, body: String) {
 		val message = SimpleMailMessage()
 		message.setTo(to)
 		message.from = "alkfejl23@alkfejl2023.org"
 		message.subject = subject
 		message.text = body
-		mailSender.send(message)
+
+		try {
+			mailSender.send(message)
+			logger.info("Email sent")
+		} catch (e: MailException) {
+			// toString required: there is a throwable .error overload
+			logger.error("Email send failed ({})", e.toString())
+		}
 	}
 }
