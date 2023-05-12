09cd4a9700d1de36f5bd1ee597004a7f6f1e8367
asztrikx <asztrik35@gmail.com>
2023-05-13 01:38:12 +0200

    remove unused imports

--- backend/src/main/kotlin/hu/vecsesiot/backend/bus/BusService.kt
+++ backend/src/main/kotlin/hu/vecsesiot/backend/bus/BusService.kt
@@ -2,12 +2,8 @@
 
 package hu.vecsesiot.backend.bus
 
-import hu.vecsesiot.backend.faultticket.FaultTicket
-import hu.vecsesiot.backend.timetable.Timetable
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
-import kotlin.jvm.optionals.getOrNull
 
 @Service
 class BusService {
