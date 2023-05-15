2340efbe74ea38d81c4f7fa5c4d8b99fd6cdc112
potyok <potyok.csaba@edu.bme.hu>
2023-05-15 19:10:01 +0200

    handle null exception from service

--- backend/src/main/kotlin/hu/vecsesiot/backend/faultticket/FaultTicketController.kt
+++ backend/src/main/kotlin/hu/vecsesiot/backend/faultticket/FaultTicketController.kt
@@ -3,6 +3,8 @@ package hu.vecsesiot.backend.faultticket
 
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -13,8 +15,18 @@ class FaultTicketController {
 
 
 	@PostMapping("/create")
-	fun createFaultTicket(@RequestBody ticket: FaultTicket) = service.createTicket(ticket)
+	fun createFaultTicket(@RequestBody ticket: FaultTicket): ResponseEntity<Nothing> = try {
+		service.createTicket(ticket)
+		ResponseEntity.ok().build()
+	} catch (e: Exception) {
+		ResponseEntity.status(HttpStatus.BAD_REQUEST).build()
+	}
 
 	@PostMapping("/{id}/refresh/{state}")
-	fun refreshState(@PathVariable id : Long, @PathVariable state : FaultTicket.State) = service.refreshState(id, state)
+	fun refreshState(@PathVariable id : Long, @PathVariable state : FaultTicket.State) : ResponseEntity<Nothing> = try {
+		service.refreshState(id, state)
+		ResponseEntity.ok().build()
+	} catch (e: Exception) {
+		ResponseEntity.status(HttpStatus.BAD_REQUEST).build()
+	}
 }
